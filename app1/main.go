0f3ea3734dbb45d51b06807977f4208ae2577832
Ali Mahmoodi <alimahmoodi@Alis-Air.fritz.box>
2024-11-30 12:29:01 +0100

    add sonarqube sast to pipeline

--- app1/main.go
+++ app1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -9,7 +10,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "There's no such thing as a bad day when you're fishing.")
 }
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok"))
+}
+
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/healthz", healthHandler) // Health check endpoint
+
+	port := ":8080"
+	log.Printf("Starting server on port %s\n", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Failed to start server: %v\n", err)
+	}
 }
+
