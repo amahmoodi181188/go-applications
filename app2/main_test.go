e384899303eecc4c5f3088618f0ca7d64808f5a2
Ali Mahmoodi <alimahmoodi@Alis-Air.fritz.box>
2024-12-01 11:59:06 +0100

    add unit test to go applications and update workflow file to solve not-computed flag

---
+++ app2/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func TestHealthHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/healthz", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	healthHandler(rr, req)
+
+	if rr.Body.String() != "ok" {
+		t.Errorf("HealthHandler returned unexpected body: got %v want %v", rr.Body.String(), "ok")
+	}
+}
